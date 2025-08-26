5afb68d70772a8d4a7e6243b1e0b49cee812e15b
dami <dami@debian.debian.com>
2025-08-26 13:14:11 -0400

    fix: allow netlify dev server

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,16 @@ export default defineConfig(({ mode }) => {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
+      server: {
+        host: true,
+        hmr: {
+            host: 'localhost',
+            protocol: 'ws',
+        },
+        allowedHosts: [
+          "devserver-master--fascinating-gingersnap-3a87fc.netlify.app"
+        ]
+      },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
